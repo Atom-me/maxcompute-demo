6231ef5faac1fe9cec02b5289d37de3946567cbb
Atom <sarming@126.com>
2023-09-04 11:14:40 +0800

    test

--- src/main/java/com/atom/maxcompute/Constant.java
+++ src/main/java/com/atom/maxcompute/Constant.java
@@ -5,8 +5,8 @@ package com.atom.maxcompute;
  */
 public interface Constant {
 
-    String accessId = "LTAI5tPJnMkYedmroXtqVfe1";
-    String accessKey = "TMi9o3FlnPdzOiHOTtrhT8dj2ou4BD";
+    String accessId = "sfasd";
+    String accessKey = "asdfasd";
 
     String endpoint = "http://service.cn-beijing.maxcompute.aliyun.com/api";
 }
